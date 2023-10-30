ef351c7106361937311cd9b579cddef4fb9c2be7
sprihari <rilind.harizaj@stud.uni-due.de>
2023-10-30 13:40:38 +0100

    The persons table command is removed from the psql

--- database.sql
+++ database.sql
@@ -2,14 +2,6 @@
 # Database setup for DevContainer
 */
 
-CREATE TABLE Persons (
-    PersonID int,
-    LastName varchar(255),
-    FirstName varchar(255),
-    Address varchar(255),
-    City varchar(255)
-);
-
 CREATE TABLE Articles (
     URLId text,
     Headline text,
