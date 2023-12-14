414e5868003dd17034c9ffcd8f1f244c26cd18e2
Max Lütkemeyer <Max.Luetkemeyer@outlook.de>
2023-12-14 14:35:12 +0000

    start cnn refactor

--- app/database/database.py
+++ app/database/database.py
@@ -4,6 +4,7 @@ import os
 DATABASE_PORT = os.environ.get("DATABASE_PORT", "5432")
 DATABASE_HOST = os.environ.get("DATABASE_HOST", "localhost")
 
+
 class Database:
     def open_connection(self):
         self.connection = psycopg2.connect(
@@ -19,8 +20,13 @@ class Database:
 
         self.cursor = self.connection.cursor()
 
-    def execute(self, sqlStatement, valuesTuple):
+    def execute(self, sqlStatement: str, valuesTuple: tuple):
         self.cursor.execute(sqlStatement, valuesTuple)
 
     def close_connection(self):
         self.connection.close()
+
+    def initialize_schema(self):
+        schema = open("./database.sql", "r").read()
+
+        self.execute(schema, ())
---
+++ app/test_cnn.py
@@ -0,0 +1,22 @@
+import asyncio
+
+from database.database import Database
+from news_scrape.cnn.cnn_etl import cnn_etl
+
+
+def manual_db_init():
+    database = Database()
+    database.open_connection()
+    database.initialize_schema()
+    database.close_connection()
+    print("DB initialized")
+
+async def main():
+    manual_db_init()
+
+    cnn_etl()
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
+    print('test')
