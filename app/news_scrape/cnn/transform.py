6c6f2f460c02899a97b5169954c7d7b8bf9be974
lindiprograms <103070245+ri-lind@users.noreply.github.com>
2023-12-19 09:13:36 +0100

    Pasted from local

--- app/news_scrape/cnn/transform.py
+++ app/news_scrape/cnn/transform.py
@@ -1,12 +1,14 @@
+from gettext import translation
 import pandas as pd
 from sqlalchemy import create_engine
 
-from database.models.article import Article
+from article import Article
 
+engine = create_engine(f'postgresql://postgres:postgres@localhost:5432/postgres')
 
-def transform(articles: list[Article]) -> list[Article]:
+def transform(articles: list[Article]) -> pd.DataFrame:
     # filtered against existing articles in database
-    new_articles = filter_articles(articles, get_engine())
+    new_articles = filter_articles(articles)
 
     # this dictionary only contains the articles that are not in the database already
     article_dicts = [article.__dict__ for article in new_articles]
@@ -14,32 +16,19 @@ def transform(articles: list[Article]) -> list[Article]:
     # this dataframe contains the data to be inserted to the articles table
 
     articles_dataframe = pd.DataFrame(article_dicts)
+    articles_dataframe.columns = articles_dataframe.columns.str.lower()
     # change the name of url to urlid
-    articles_dataframe["urlid"] = articles_dataframe["url"]
-    articles_dataframe = articles_dataframe.drop(columns=["url"])
-    # change the name of description to image description
-    articles_dataframe["imagedescription"] = articles_dataframe["description"]
-    articles_dataframe = articles_dataframe.drop(columns=["description"])
-    # change the name of upload_timestamp to uploadtimestamp
-    articles_dataframe["uploadtimestamp"] = articles_dataframe["upload_timestamp"]
-    articles_dataframe = articles_dataframe.drop(columns=["upload_timestamp"])
-    # change the name of upload_timestamp to uploadtimestamp
-    articles_dataframe["scrapingtimestamp"] = articles_dataframe["scraping_timestamp"]
-    articles_dataframe = articles_dataframe.drop(columns=["scraping_timestamp"])
-    # fix imageurl, for some reason this column is not recognized correctly
-    articles_dataframe["imageurl"] = articles_dataframe["imageUrl"]
-    articles_dataframe = articles_dataframe.drop(columns=["imageUrl"])
-    # drop the read_time column
-    articles_dataframe = articles_dataframe.drop(columns=["read_time"])
-    print(articles_dataframe["urlid"])
-    # filtered the duplicates out
-    print(articles_dataframe.shape)
+    articles_dataframe.rename(columns=
+        {
+            'upload_timestamp' : 'uploadtimestamp'
+        }
+    )
+    print(articles_dataframe.info())
     articles_dataframe.drop_duplicates(subset=["urlid"], keep="first", inplace=True)
-    print(articles_dataframe.shape)
 
-    return articles
+    return articles_dataframe
 
-def filter_articles(articles: list[Article], engine) -> list[Article]:
+def filter_articles(articles: list[Article]) -> list[Article]:
     new_articles = []
     articles_already_present = pd.read_sql_table("articles", con=engine)
     #  print(articles_already_present.info()) # logging
@@ -55,5 +44,3 @@ def filter_articles(articles: list[Article], engine) -> list[Article]:
 
     return new_articles
 
-def get_engine():
-    return create_engine(f'postgresql://postgres:postgres@localhost:5432/postgres')
