d0f82f428a83a398e248407b10003b47d17496d7
lkraemer <lkraemer@uni-muenster.de>
2024-01-11 13:07:18 +0100

    clustering as etl pipeline

---
+++ app/news_clustering/load.py
@@ -0,0 +1,35 @@
+
+# ---------- IMPORTS ---------------
+
+import sys
+sys.path.insert(0,'/workspaces/data-app/app/database')
+
+# basic imports
+import psycopg2
+import pandas as pd
+import numpy as np
+import pandas.io.sql as sqlio
+from database import Database # type: ignore
+
+
+# -------------- LOAD ----------------
+
+def load_clusters_into_db(articles):
+    # new database connection
+    database = Database()
+    database.open_connection()
+
+    # write clusterIds and clusterTopics into database
+    for index, row in articles.iterrows():
+        try:
+            database.execute('''UPDATE articles
+                                SET clusterId = %s, clusterTopic = %s
+                                WHERE urlid = %s''',
+                                (row.clusterId,
+                                 row.clusterTopic,
+                                 row.urlid
+                                )
+                            )
+        except BaseException as ex:
+            print('Error: ', ex)
+    database.close_connection()
