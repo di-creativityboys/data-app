0ef25677036e1afe9bac59ac69acc3e90805373c
Max Lütkemeyer <Max.Luetkemeyer@outlook.de>
2024-01-26 21:40:29 +0000

    cnn database depend on environment variables

--- app/news_scrape/cnn/load.py
+++ app/news_scrape/cnn/load.py
@@ -1,8 +1,11 @@
-from news_scrape.cnn.article import Article
 import pandas as pd
 from sqlalchemy import create_engine
+import os
+
+DATABASE_PORT: str = os.environ.get("DATABASE_PORT", "5432")
+DATABASE_HOST: str = os.environ.get("DATABASE_HOST", "localhost")
 
 
 def load(articles: pd.DataFrame):
-    engine = create_engine(f"postgresql://postgres:postgres@localhost:5432/postgres")
+    engine = create_engine(f"postgresql://postgres:postgres@{DATABASE_HOST}:{DATABASE_PORT}/postgres")
     articles.to_sql(name="articles", con=engine, if_exists="replace", index=False)
